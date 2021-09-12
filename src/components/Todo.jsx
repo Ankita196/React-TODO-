784cb52e4807caaba714ac23242e684551378609
Ankita196 <muskanmakde1999@gmail.com>
2021-09-12 12:55:32 +0530

    1 file modified

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
-import DirectionsIcon from '@material-ui/icons/Directions';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   paper: {
     padding: theme.spacing(2),
@@ -19,11 +22,14 @@ const useStyles = makeStyles((theme) => ({
     fontWeight:"bold",
     fontSize:26
   },
+
+  
   roots: {
     padding: '2px 4px',
     display: 'flex',
     alignItems: 'center',
     width: 400,
+    
   },
   input: {
     marginLeft: theme.spacing(1),
@@ -42,25 +48,37 @@ export default function Todo() {
   const classes = useStyles();
   return (
     <div>
-      <Grid container spacing={3}  style={{display:"block",marginLeft:"auto",marginRight:"auto"}}>
+      <Grid container spacing={5} className={classes.root}>
         <Grid item xs={12} className={classes.paper}>
         Add your Todo here 
         </Grid>
-        <Grid xs={12} style={{alignItems:"center"}} >
+        <Grid xs={12} className={classes.paper}>
         <Paper component="form" className={classes.roots}>
       
       <InputBase
         className={classes.input}
         placeholder="Add your Todo"
-       
+       type="text"
       />
       
       <Divider className={classes.divider} orientation="vertical" />
-      <IconButton color="primary" className={classes.iconButton} aria-label="directions">
+      <IconButton color="primary" className={classes.iconButton} aria-label="AddIcon">
         <AddIcon  />
       </IconButton>
     </Paper>
         </Grid>
+        <Grid xs={12} className={classes.paper} >
+        <Paper component="form" className={classes.roots} style={{backgroundColor:"#ffe0b2"}}>
+      
+        <Typography style={{width:500}}>apple</Typography>
+      
+      <Divider className={classes.divider} orientation="vertical" />
+      <IconButton color="primary" className={classes.iconButton} style={{margineRight:20}} >
+        <DeleteIcon  />
+      </IconButton>
+    </Paper>
+        </Grid>
+        
         </Grid>
 
     </div>
